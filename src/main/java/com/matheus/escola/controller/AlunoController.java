3f00bfd5ea9574a372b4186d57eaa90fdb955021
Matheus Cabral <matheusstcabral@gmail.com>
2025-02-10 22:14:22 -0300

    feat: criar rota para criacao de aluno

--- src/main/java/com/matheus/escola/controller/AlunoController.java
+++ src/main/java/com/matheus/escola/controller/AlunoController.java
@@ -1,14 +1,13 @@
 package com.matheus.escola.controller;
 
+import com.matheus.escola.DTO.AlunoRequestDTO;
 import com.matheus.escola.DTO.AlunoResponseDTO;
 import com.matheus.escola.model.Aluno;
-import com.matheus.escola.model.Turma;
-import com.matheus.escola.repository.AlunoRepository;
+import com.matheus.escola.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,27 +17,23 @@ import java.util.List;
 public class AlunoController {
 
     @Autowired
-    private AlunoRepository alunoRepository;
+    private AlunoService service;
 
     @GetMapping
     public ResponseEntity<List<AlunoResponseDTO>> listarAlunos(){
 
-        List<AlunoResponseDTO> alunos = alunoRepository.findAll()
-                .stream()
-                .map(aluno -> new AlunoResponseDTO(
-                        aluno.getId(),
-                        aluno.getNome(),
-                        aluno.getRa(),
-                        aluno.getDataNascimento(),
-                        aluno.getTurma().getId()
-                ))
-                .toList();
+        List<AlunoResponseDTO> alunos = service.listarAlunos();
 
         if (alunos.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-
         return ResponseEntity.ok().body(alunos);
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoResponseDTO> criarAluno(@RequestBody AlunoRequestDTO alunoRequest){
+        AlunoResponseDTO alunoResponse = service.criarAluno(alunoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoResponse);
     }
 
 }
