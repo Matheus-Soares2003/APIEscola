f66b5bf3d26bb845f0c15e1cce93424aceb3b812
Matheus Cabral <matheusstcabral@gmail.com>
2025-02-10 22:13:52 -0300

    feat: criar service de alunos

---
+++ src/main/java/com/matheus/escola/service/AlunoService.java
@@ -0,0 +1,61 @@
+package com.matheus.escola.service;
+
+import com.matheus.escola.DTO.AlunoRequestDTO;
+import com.matheus.escola.DTO.AlunoResponseDTO;
+import com.matheus.escola.model.Aluno;
+import com.matheus.escola.model.Turma;
+import com.matheus.escola.repository.AlunoRepository;
+import com.matheus.escola.repository.TurmaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Autowired
+    private TurmaRepository turmaRepository;
+
+    public AlunoResponseDTO criarAluno(AlunoRequestDTO alunoRequest) throws RuntimeException {
+
+        Turma turma = turmaRepository.findById(alunoRequest.turmaID())
+                .orElseThrow(() -> new RuntimeException("Turma não encontrada"));
+
+        Aluno aluno = new Aluno();
+        aluno.setNome(alunoRequest.nome());
+        aluno.setRa(alunoRequest.ra());
+        aluno.setDataNascimento(alunoRequest.dataNascimento());
+        aluno.setTurma(turma);
+
+        Aluno alunoCriado =  alunoRepository.save(aluno);
+
+        return new AlunoResponseDTO(
+                alunoCriado.getId(),
+                alunoCriado.getNome(),
+                alunoCriado.getRa(),
+                alunoCriado.getDataNascimento(),
+                alunoCriado.getTurma().getId()
+        );
+
+    }
+
+    public List<AlunoResponseDTO> listarAlunos(){
+        List<AlunoResponseDTO> alunos = alunoRepository.findAll()
+                .stream()
+                .map(aluno -> new AlunoResponseDTO(
+                        aluno.getId(),
+                        aluno.getNome(),
+                        aluno.getRa(),
+                        aluno.getDataNascimento(),
+                        aluno.getTurma().getId()
+                ))
+                .toList();
+
+        return alunos;
+    }
+}
