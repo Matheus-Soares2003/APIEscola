53aeaa1ca5d489e3d2fefafe48b29ed601185214
Matheus Cabral <matheusstcabral@gmail.com>
2025-02-14 23:23:23 -0300

    refactor: transformar AlunoResponseDTO de classe para record e adicionar atributo TurmaResponseDTO

--- src/main/java/com/matheus/escola/DTO/AlunoResponseDTO.java
+++ src/main/java/com/matheus/escola/DTO/AlunoResponseDTO.java
@@ -3,59 +3,19 @@ package com.matheus.escola.DTO;
 import com.matheus.escola.model.Turma;
 import java.time.LocalDate;
 
-public class AlunoResponseDTO {
+public record AlunoResponseDTO (
+    Long id,
+    String nome,
+    String ra,
+    LocalDate dataNascimento,
+    TurmaResponseDTO turma
+){
 
-    private Long id;
-    private String nome;
-    private String ra;
-    private LocalDate dataNascimento;
-    private Long turmaId;
-
-    public AlunoResponseDTO(Long id, String nome, String ra, LocalDate dataNascimento, Long turmaId) {
+    public AlunoResponseDTO(Long id, String nome, String ra, LocalDate dataNascimento, TurmaResponseDTO turma) {
         this.id = id;
         this.nome = nome;
         this.ra = ra;
         this.dataNascimento = dataNascimento;
-        this.turmaId = turmaId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRa() {
-        return ra;
-    }
-
-    public void setRa(String ra) {
-        this.ra = ra;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public Long getTurmaId() {
-        return turmaId;
-    }
-
-    public void setTurmaId(Long turmaId) {
-        this.turmaId = turmaId;
+        this.turma = turma;
     }
 }
